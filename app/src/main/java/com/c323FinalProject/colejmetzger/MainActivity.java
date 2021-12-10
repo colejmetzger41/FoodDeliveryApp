9f946d07c59389a583dbb7a0327d0d838da8efc1
colejmetzger41 <colejmetzger@gmail.com>
2021-12-10 13:00:53 -0500

    Initial commit

---
+++ app/src/main/java/com/c323FinalProject/colejmetzger/MainActivity.java
@@ -0,0 +1,14 @@
+package com.c323FinalProject.colejmetzger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
